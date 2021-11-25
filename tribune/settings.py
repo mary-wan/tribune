cc1a803ac6d85b5789a5fb7978fad7d945f436bc
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-25 12:55:22 +0300

    add version

--- tribune/settings.py
+++ tribune/settings.py
@@ -17,7 +17,6 @@ from decouple import config,Csv
 
 
 MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # development
 if config('MODE')=='dev':
@@ -36,7 +35,7 @@ if config('MODE')=='dev':
 else:
    DATABASES = {
        'default': dj_database_url.config(
-        #    default=config('DATABASE_URL')
+           default=config('DATABASE_URL')
        )
    }
 
