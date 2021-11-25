1853af81d7691f484604d1c8f8eae2f2727f8d80
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-25 14:29:38 +0300

    pull

--- tribune/settings.py
+++ tribune/settings.py
@@ -41,8 +41,8 @@ elif config('MODE')=='prod':
    }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES = { 'default': dj_database_url.config() }
-DATABASES['default'].update(db_from_env)
+DATABASES = { 'default': dj_database_url.config() }
+# DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
