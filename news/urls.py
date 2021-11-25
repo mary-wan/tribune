fe81e8d71fe9bd0a20524251bb2906c2c570ca51
mary-wan <mary.njenga@student.moringaschool.com>
2021-11-25 12:15:39 +0300

    deploy

---
+++ news/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls.conf import path
+from . import views
+urlpatterns = [
+    
+    path('', views.news_today,name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results'),
+    path('article/<int:article_id>/',views.article,name ='article'),
+    # url(r'^article/(\d+)',views.article,name ='article'),
+    # url(r'^$', views.news_today, name = 'newsToday'),
+    # url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),
+    # url(r'^search/', views.search_results, name='search_results')
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
